f33336b110409dbb3cb0d0c4721e70ef425e79bb
sohamdatey <sohamdatey22@gmail.com>
2023-03-20 10:35:53 +0530

    pagination added

--- app/typeTest.ts
+++ app/typeTest.ts
@@ -1,21 +1,21 @@
 import { S3Client } from '@aws-sdk/client-s3';
 import { GetObjectCommand } from "@aws-sdk/client-s3";
-import { DynamoDBClient} from '@aws-sdk/client-dynamodb'
-import { DynamoDBDocumentClient ,QueryCommand  } from "@aws-sdk/lib-dynamodb"; // ES6 import
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb'
+import { DynamoDBDocumentClient, QueryCommand } from "@aws-sdk/lib-dynamodb"; // ES6 import
 import { v4 } from 'uuid'
 
-export async function handler(event , context) {
+export async function handler(event, context) {
     console.log('uuid', v4())
-    console.log('event: ',JSON.stringify(event)) 
-    console.log(event.Records[0]?.s3.bucket.name,event.Records[0]?.s3.object.key)
-    await run(event.Records[0]?.s3.bucket.name,event.Records[0]?.s3.object.key);
+    console.log('event: ', JSON.stringify(event))
+    console.log(event.Records[0]?.s3.bucket.name, event.Records[0]?.s3.object.key)
+    await run(event.Records[0]?.s3.bucket.name, event.Records[0]?.s3.object.key);
     return {
         statusCode: 200
     }
 }
 
 
-export const run = async (bucketName : string, fileName) => {
+export const run = async (bucketName: string, fileName) => {
     try {
         const REGION = "us-east-1";
         const s3Client = new S3Client({ region: REGION });
@@ -27,11 +27,11 @@ export const run = async (bucketName : string, fileName) => {
         // Get the object from the Amazon S3 bucket. It is returned as a ReadableStream.
         const data = await s3Client.send(new GetObjectCommand(bucketParams));
         // Convert the ReadableStream to a string.
-        const filedata : Uint8Array = await data.Body.transformToByteArray();
+        const filedata: Uint8Array = await data.Body.transformToByteArray();
 
-        let stringdata : string;       
+        let stringdata: string;
         filedata.forEach(element => {
-            stringdata+= String.fromCharCode(element);
+            stringdata += String.fromCharCode(element);
         });
         console.log(Buffer.from(filedata).toString('base64'))
         // console.log(filedata)
@@ -44,32 +44,40 @@ export const run = async (bucketName : string, fileName) => {
 
 export const dynamoTest = async () => {
 
-    // aws dynamodb query \
-    // --table-name Reply \
-    // --key-condition-expression "Id = :id and begins_with(ReplyDateTime, :dt)" \
-    // --expression-attribute-values  file://values.json
-
-    const client = new DynamoDBClient({
-        region : 'us-east-1'
-    });
-    const ddbDocClient = DynamoDBDocumentClient.from(client); // client is DynamoDB client
-    
-    const command = new QueryCommand({
-        TableName: "NqAdminRequestTable",
+    const queryParams = {
+        TableName: "Test",
         ExpressionAttributeNames: {
-          "#PK": "PK",
-          "#SK": "SK"
+            "#PK": "PK",
+            "#SK": "SK"
         },
         ExpressionAttributeValues: {
-          ":CONTRACT": 'contractId#123456',
-          ":EMP": "empId#1234",
+            ":CONTRACT": 'contractId#123456',
+            ":EMP": "empId#1234",
         },
         //FilterExpression: "#PK = :CONTRACT",
-        KeyConditionExpression: "#PK = :CONTRACT and begins_with(#SK,:EMP)",
-      });
+        KeyConditionExpression: "#PK = :CONTRACT and begins_with(#SK,:EMP)"
+    }
 
-      let a =  await ddbDocClient.send(command);
-      console.log(a)
+    let results = [];
+    let LastEvaluatedKey = await queryAndAddToResults(queryParams, results);
+    while (LastEvaluatedKey) {
+        LastEvaluatedKey = await queryAndAddToResults({
+            ...queryParams,
+            ExclusiveStartKey: LastEvaluatedKey
+        }, results)
+    }
+    console.log(results, results.length)
+
 };
-//console.log('hello')
+const queryAndAddToResults = async (query, results) => {
+    const client = new DynamoDBClient({
+        region: 'us-east-1'
+    });
+    const ddbDocClient = DynamoDBDocumentClient.from(client); // client is DynamoDB client
+    const command = new QueryCommand(query);
+    let more = await ddbDocClient.send(command);
+    results.push(...more.Items);
+    return more.LastEvaluatedKey;
+}
+
 dynamoTest();
